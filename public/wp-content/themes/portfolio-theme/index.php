1b8fb3bf52cd8fc1de33d03f1aabbe533c867dfc
Dolma Sherpa <dolma2000@gmail.com>
2020-07-18 10:44:04 +0900

    Added .gitignore

--- public/wp-content/themes/portfolio-theme/index.php
+++ public/wp-content/themes/portfolio-theme/index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Final Project</title>
-</head>
-<body>
-IDM250
-</body>
-</html>
+<?php get_header();?>
+
+<p>IDM250</p>
+
+<?php get_footer(); ?>
